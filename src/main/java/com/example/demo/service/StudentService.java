027179f91edec00520a26d60defe54f54b801569
bala-amypo <bala@amypo.in>
2025-12-17 09:20:29 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,11 +1,18 @@
+
 package com.example.demo.service;
 
-import com.example.demo.entity.StudentEntity;
 import java.util.List;
+
+import com.example.demo.entity.StudentEntity;
 
 public interface StudentService {
 
     public StudentEntity addStudents(StudentEntity student);
-    public List<StudentEntity> getStudent();
+
+    public List<StudentEntity> getStudents();
+
+    public StudentEntity getStudentById(Long id);
+
+    public void deleteStudentById(Long id);
 
 }
--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -1,12 +1,13 @@
 package com.example.demo.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.StudentEntity;
 import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
-import java.util.List;
 
 @Service
 public class StudentServiceImpl implements StudentService {
@@ -20,9 +21,18 @@ public class StudentServiceImpl implements StudentService {
     }
 
     @Override
-    public List<StudentEntity> getStudent()
-    {
+    public List<StudentEntity> getStudents() {
         return repo.findAll();
+    }
+
+    @Override
+    public StudentEntity getStudentById(Long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deleteStudentById(Long id) {
+        repo.deleteById(id);
     }
 
 }
