59c630acfb7bfd67e0e42c52c86eb4f66b07c69c
bala-amypo <bala@amypo.in>
2025-12-17 10:41:02 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -4,48 +4,50 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 @Entity
-public class StudentEntity{
+public class StudentEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private float cgpa;
 
-    // public void setId(Long id){
-    //     this.id = id;
-    // }
-    // public Long getId(){
-    //     return id;
-    // }
+    public StudentEntity() {}
 
-    public void setName(String name){
+    public StudentEntity(String name, String email, float cgpa) {
         this.name = name;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setEmail(String email){
         this.email = email;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public void setCgpa(float cgpa){
         this.cgpa = cgpa;
     }
-    public float getCgpa(){
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public float getCgpa() {
         return cgpa;
     }
-    StudentEntity(){}
-    StudentEntity(String name , String email,float cgpa){
-        // this.id = id;
-        this.name=name;
-        this.email=email;
-        this.cgpa=cgpa;
+
+    public void setCgpa(float cgpa) {
+        this.cgpa = cgpa;
     }
-
-
 }
