edb782d9605d06eacf6951dfd19ab1fecc5b57fb
bala-amypo <bala@amypo.in>
2025-12-25 05:04:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -4,9 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import com.example.demo.entity.StudentEntity;
+import java.util.List;
 
 public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
     
-    @Query("FROM StudentEntity s WHERE s.cgpa > :cgpa");
-    List<SudentEntity> findStudentWithCgpa(@Param("cgpa") float cgpa);
+    @Query("FROM StudentEntity s WHERE s.cgpa > :cgpa")
+    List<StudentEntity> findStudentWithCgpa(@Param("cgpa") float cgpa);
 }
